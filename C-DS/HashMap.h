c53c4f290d4746eb710a827920c6f3a573ccf82b
Kareem Hatem <kareemhatem102@gmail.com>
2024-05-19 07:20:04 +0300

    added hash table and hash map

--- C-DS/HashMap.h
+++ C-DS/HashMap.h
@@ -4,14 +4,14 @@
 #include <vector>
 
 
-class HashTable :
+class HashMap :
     public GrandWindow
 {
     // grid size constraints:
     #define MAX_TABLE_SIZE 50
     #define MIN_TABLE_SIZE 2
 
-    #define HT_SEPARATOR_SIZE 100.f * zoomScale
+    #define HM_SEPARATOR_SIZE 100.f * zoomScale
     #define CELL_SIZE_X 180.f * zoomScale
     #define CELL_SIZE_Y 60.f * zoomScale
 
@@ -21,6 +21,7 @@ class HashTable :
 
     #define VERTEX_RADIUS 30.f * zoomScale
     #define NODES_DIST 200.f * zoomScale
+    #define LINK_DIST 120.f
     #define DEFAULT_NODE_COL ImGui::GetColorU32(IM_COL32(150, 150, 150, 255))
     #define ITER_NODE_COL ImGui::GetColorU32(IM_COL32(50, 150, 150, 255))
     #define FAIL_NODE_COL ImGui::GetColorU32(IM_COL32(150, 50, 50, 255))
@@ -28,10 +29,10 @@ class HashTable :
     #define DEFAULT_EDGE_COL ImGui::GetColorU32(IM_COL32(200, 200, 200, 255))
     #define TEXT_COL ImGui::GetColorU32(IM_COL32(255, 255, 255, 255))
 
-        // speed constraints:
-    #define HT_MAX_SPEED 5.0f
-    #define HT_MIN_SPEED 0.5f
-    #define HT_DELAY 1.f
+    // speed constraints:
+    #define HM_MAX_SPEED 5.0f
+    #define HM_MIN_SPEED 0.5f
+    #define HM_DELAY 1.f
 
 private:
     // private fields:
@@ -41,30 +42,32 @@ private:
     const ImGuiViewport* viewport = ImGui::GetMainViewport();
     const ImGuiIO* io = &ImGui::GetIO();
 
-    int cur_tool = 0, table_size_slider = 10,  table_size = 10, cur_bucket = -1, mode = 0;
+    int cur_tool = 0, table_size_slider = 10, table_size = 10, cur_bucket = -1, mode = 0;
     float speed = 1.f, curTime = 0;
     bool paused = false, camFollow = false, movingCam = false, found = false;;
     ImVec2 camPos = { 0, 0 }, camTarget = { 0, 0 };
     ImVec2 center = { 0, 0 };
-    std::string searchVal, toDelete;
+    std::string searchKey, newVal, toDelete;
 
     struct Node {
-        std::string value;
+        std::string key, value;
         Node* next;
         ImVec2 curPos;
-        ImU32 color;
-        Node(std::string value, ImVec2 curPos) {
+        ImU32 keyColor;
+        ImU32 valColor;
+        Node(std::string key, std::string value, ImVec2 curPos) {
+            this->key = key;
             this->value = value;
             this->curPos = curPos;
             next = nullptr;
-            color = DEFAULT_NODE_COL;
+            keyColor = valColor = DEFAULT_NODE_COL;
         }
     };
 
     std::vector<Node*> buckets;
-    Node* iteratingNode = nullptr, *tempNode = nullptr;
+    Node* iteratingNode = nullptr, * tempNode = nullptr;
 
-    char add_node_text[10] = "";
+    char key_text[10] = "", value_text[10] = "";
 
     // private methods:
     void controlsUpdate();
@@ -76,8 +79,8 @@ private:
 
 public:
 
-    HashTable(std::string, int&, float&, bool&);
-    ~HashTable();
+    HashMap(std::string, int&, float&, bool&);
+    ~HashMap();
 
     // public methods:
     void update();
